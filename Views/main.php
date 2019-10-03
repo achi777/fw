1d37b048e8c83125669452f06d97b0d5fa956524
achi <archil.odishelidze@gmail.com>
2019-10-03 11:34:45 +0400

    second upload

--- Views/main.php
+++ Views/main.php
@@ -14,8 +14,8 @@
         <div class="row">
             <@foreach ($members AS $item):@>
             <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <h2>{{$item->title}}</h2>
+                <p>{{$item->description}}</p>
                 <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <@endforeach@>
