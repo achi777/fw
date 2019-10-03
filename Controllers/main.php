41e3072b03f6aa2130abed7bfe6b9942d1da9eb9
achi <archil.odishelidze@gmail.com>
2019-10-03 10:54:22 +0400

    first upload

--- Controllers/main.php
+++ Controllers/main.php
@@ -17,7 +17,6 @@ class Controller extends init
 
     public function main()
     {
-        //$data['olt_swiths'] = $this->model->olt_swiths();
         //echo $this->model->seletcList();
         $data['title'] = "main page";
         $data['members'] = $this->model->seletcList();
