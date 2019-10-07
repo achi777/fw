9b04f634e5e3ca56a9d07ab691353ce9eb25b974
achi <archil.odishelidze@gmail.com>
2019-10-07 13:40:23 +0400

    update easy lib

--- lib/core/easy.class.php
+++ lib/core/easy.class.php
@@ -7,4 +7,15 @@ class easy{
             echo $var;
         }
     }
+
+    public static function uuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
 }
